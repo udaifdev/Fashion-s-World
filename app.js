7d2fe4ef49b7ceb28a22be62cd738b91a304100c
Muhammad <muhammadudaif786udaif@gmail.com>
2024-06-03 20:26:54 +0530

    check

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ app.use(express.static(__dirname + "/public"))
 console.log(__dirname, '>>>>>>>>>')
 app.get('/favicon.ico', (req, res) => res.status(204).end());
 
+app.set('Views', path.join(__dirname, 'Views'));
+
 // view engine 
 app.set("view engine", "ejs");
 
